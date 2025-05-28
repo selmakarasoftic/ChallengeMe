8632b2a287139d64fb83390ccde7d5b8339a065e
Selma <selma.karasoftic@stu.ibu.edu.ba>
2025-05-28 13:54:43 +0200

    fixing

---
+++ app/src/main/java/com/example/challengeme/AppNavigation.kt
@@ -0,0 +1,18 @@
+package com.example.challengeme
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(navController = navController)
+        }
+        composable("register") {
+            RegisterScreen(navController = navController)
+        }
+    }
+}
--- app/src/main/java/com/example/challengeme/MainActivity.kt
+++ app/src/main/java/com/example/challengeme/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.challengeme
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.rememberNavController
+import com.example.challengeme.ui.theme.ChallengeMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen()
+            ChallengeMeTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    AppNavigation(navController)
+                }
+            }
         }
     }
 }
